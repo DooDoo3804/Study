b056b2c18183e7cc0246b34e337b1ea32c544e45
DooDoo3804 <doodoo3804@gmail.com>
2022-07-29 17:41:08 +0900

    edit 구현04

--- coding_test/구현/구현04.py
+++ coding_test/구현/구현04.py
@@ -18,4 +18,16 @@ count = 0
 turn_count = 0
 
 while True:
+    turn_left()
+    nx = x + move_list[direction][1]
+    ny = y + move_list[direction][0]
+    turn_count += 1
+    if player_map[nx][ny] != 1 and init_map[nx][ny] != 1 :
+        x = nx
+        y = ny
+        count += 1
+    elif turn_count >= 4:
+        x += move_list[direction-2][1]
+        y += move_list[direction-2][0]
+    player_map[x][y] = 1
     pass
