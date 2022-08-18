952222acba1eb0d8248d22027bb3aafbb88bc594
DooDoo3804 <doodoo3804@gmail.com>
2022-08-18 17:51:47 +0900

    TIL 0818

--- BOJ/2300_기지국/2300_기지국.py
+++ BOJ/2300_기지국/2300_기지국.py
@@ -1,6 +1,3 @@
-# 2300_기지국
-# 2022-08-18
-
 import sys
 sys.stdin = open('input.txt', 'r')
 T = int(input())
@@ -13,15 +10,18 @@ for i in range(T):
     for j in range(i):
         if coord[j][0] > coord[i][0]:
             coord[j], coord[i] = coord[i], coord[j]
-x1 = x2 = y1 = y2 = result = 0
+x1 = x2 = y1 = y2 = result = width = lastx = lasty = lastwidth = 0
 for i in coord:
     x2 = i[0]
     y2 = i[1]
     width = x2 - x1
     height = y2 - y1
     if x1 == 0 and y1 == 0:
-        if coord.index[i] == T - 1:
-            result
+        if i == coord[-1]:
+            result -= lastwidth
+            width = x2 - lastx
+            result += width
+            break
         x1 = x2
         y1 = y2
         continue
@@ -31,5 +31,8 @@ for i in coord:
         y2 = - y2
     if width/2 >= y2:
         result += width
+        lastwidth = width
+        lastx = x1
+        lasty = y1
         x1 = x2 = y1 = y2 = 0
 print(result)
