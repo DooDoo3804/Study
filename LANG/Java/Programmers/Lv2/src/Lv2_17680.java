07f5386fdcde0ccede3dd215e7db15c924d007e5
DooDoo3804 <doodoo3804@gmail.com>
2022-12-10 23:30:58 +0900

    TIL 1210

--- LANG/Java/Programmers/Lv2/src/Lv2_17680.java
+++ LANG/Java/Programmers/Lv2/src/Lv2_17680.java
@@ -1,18 +1,20 @@
-import java.util.ArrayList;
-
 public class Lv2_17680 {
     public static void main(String[] args) {
         Solution17680 s = new Solution17680();
-
+        int cacheSize = 2;
+        String[] cities = {"Jeju", "Pangyo", "NewYork", "newyork"};
+        s.solution(cacheSize, cities);
     }
 }
 
 class Solution17680 {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-
-
-
+        LRU lru = new LRU(cacheSize);
+        for (String c : cities) {
+            answer += lru.addNodes(c);
+        }
+        System.out.println(answer);
         return answer;
     }
     private static class Nodes {
@@ -23,7 +25,7 @@ class Solution17680 {
         public Nodes (String city, Nodes preNode, Nodes nextNode) {
             this.preNode = preNode;
             this.nextNode = nextNode;
-            this.city = city;
+            this.city = city.toLowerCase();
         }
     }
     private static class LRU {
@@ -39,33 +41,32 @@ class Solution17680 {
             tail.preNode = head;
         }
 
-        public void addNodes (String nodeAdd) {
+        public Integer addNodes (String nodeAdd) {
             Nodes node = this.head.nextNode;
-
             while(!node.city.equals("")) {
-                if (nodeAdd.equals(node.city)) {
+                if (nodeAdd.toLowerCase().equals(node.city)) {
                     this.cacheHit(node, nodeAdd);
-                    return;
+                    return 1;
                 }
                 node = node.nextNode;
             }
             this.cacheMiss(nodeAdd);
+            return 5;
         }
 
-        public Integer cacheHit(Nodes node, String nodeAdd) {
+        public void cacheHit(Nodes node, String nodeAdd) {
             this.removeNode(node);
             this.addFront(nodeAdd);
-            return 5;
         }
         public void cacheMiss(String nodeAdd) {
             this.addFront(nodeAdd);
             if (this.totalLen() > this.size) {
                 this.removeTail();
             }
-
         }
-        public removeTail() {
-            
+        public void removeTail() {
+            this.tail.preNode.preNode.nextNode = this.tail;
+            this.tail.preNode = this.tail.preNode.preNode;
         }
         public Integer totalLen() {
             int length = 0;
@@ -77,7 +78,8 @@ class Solution17680 {
             return length;
         }
         public void removeNode(Nodes node) {
-
+            node.nextNode.preNode = node.preNode;
+            node.preNode.nextNode = node.nextNode;
         }
         public void addFront(String nodeAdd) {
             Nodes newNode = new Nodes(nodeAdd, null, null);
