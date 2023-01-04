d512316da37c708161a65beaa6a2d444d08988cb
DooDoo3804 <doodoo3804@gmail.com>
2023-01-04 23:21:23 +0900

    TIL 0104

--- LANG/SpringBoot/employee_crud_backend/src/main/java/com/example/employee_crud_backend/controller/EmployeeController.java
+++ LANG/SpringBoot/employee_crud_backend/src/main/java/com/example/employee_crud_backend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.employee_crud_backend.controller;
 
+import com.example.employee_crud_backend.exception.ResourceNotFoundException;
 import com.example.employee_crud_backend.model.Employee;
 import com.example.employee_crud_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,23 @@ public class EmployeeController {
     @PostMapping(value = "/employees")
    public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+   }
+
+   @GetMapping(value = "/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
+   }
+
+   @PutMapping(value = "/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+       Employee employee = employeeRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+       employee.setFirstName(employeeDetails.getFirstName());
+       employee.setLastName(employeeDetails.getLastName());
+       employee.setEmail(employeeDetails.getEmail());
+
+       Employee updatedEmployee = employeeRepository.save(employee);
+       return ResponseEntity.ok(updatedEmployee);
    }
 }
