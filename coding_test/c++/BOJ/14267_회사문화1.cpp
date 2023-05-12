4304a634c1f5576897d2d2daa80bbdf3949a4543
dodo327 <doodoo3804@gmail.com>
2023-05-12 17:23:46 +0900

    TIL 0512

---
+++ coding_test/c++/BOJ/14267_회사문화1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int result[100001] = {0};
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    cin >> n >> m;
+    int parent[100001] = {0};
+    int apl[100001] = {0};
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        int sub_int = 0;
+        cin >> sub_int;
+        parent[i] = sub_int;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int a = 0;
+        int b = 0;
+        cin >> a >> b;
+        apl[a] += b;
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        if (i == 1)
+        {
+            result[i] = 0;
+        }
+        else
+        {
+            result[i] = apl[i] + result[parent[i]];
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        cout << result[i] << " ";
+    }
+    return 0;
+}
