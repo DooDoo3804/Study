c866676e72b9e33c8a69302d146422d7acddf28f
DooDoo3804 <doodoo3804@gmail.com>
2023-12-14 12:51:51 +0900

    TIL 1214 백준 15650

---
+++ coding_test/c++/BOJ/15650_n과m(2).cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<map>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int n, m;
+vector<int> q;
+
+void greedy(int now) {
+	if (q.size() == m) {
+		for (int i = 0; i < q.size(); i++) {
+			cout << q[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = now + 1; i <= n; i++) {
+		q.push_back(i);
+		greedy(i);
+		q.pop_back();
+	}
+
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n - m + 1; i++) {
+		q.push_back(i);
+		greedy(i);
+		q.pop_back();
+	}
+	
+	return 0;
+}
